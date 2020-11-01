a1dc61c48f3821aa1fb1ff77b0869acbad554a0e
imanol <imanolguzman309@gmail.com>
2020-11-01 08:20:35 -0400

    Estoy usando tkinter

--- TiendaDeRopa.py
+++ TiendaDeRopa.py
@@ -1,23 +1,5 @@
 from os import system
+from tkinter import ttk
+from tkinter import *
 
 
-class Usuarios:
-    
-    def login(self):
-        name = input("Incresa tu nombre de usuario:")
-        contra = input("Incresa tu contraseña de usuario:")
-        
-        nombre = "yordi"
-        contraseña = "852518"
-        
-        if name == nombre and contra == contraseña:
-            system("cls")
-            print("bien")
-        else:
-            
-            system("cls")
-            print('Esa no es tu cuenta correcta')
-            Usuarios1.login()
-
-Usuarios1 = Usuarios()
-Usuarios1.login()
